793c735f96dfd37fe131a59476f8a7074881d464
ojotuk <ojotuk14@yahoo.com>
2021-06-28 16:50:50 +0100

    update

--- src/components/dashboard/console/Index.js
+++ src/components/dashboard/console/Index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Statistic from './subs/statistics/Index'
-
+import NewRequest from './subs/request/Index'
 //
 export default function Index() {
   
   return (<div>
       <Statistic />
+      <NewRequest />
       Console
       
       </div>);
