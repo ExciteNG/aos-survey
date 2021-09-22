cc5a7267f30a486a5bf4325179702106d6fd24ca
ojotuk <ojotuk14@yahoo.com>
2021-09-22 13:49:13 +0100

    added loader icon

--- src/components/routes/page.js
+++ src/components/routes/page.js
@@ -1,17 +1,28 @@
 import React, { Suspense } from "react";
+import spinner from "../asset/img/spinner.svg"
 
 const Home = React.lazy(() => import("../pages/landing/Index"));
 const Auth = React.lazy(()=>import("../pages/auth/Index"));
 
-
 // 
 export const HomePage = () => (
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<FallBack />}>
     <Home />
   </Suspense>
 );
 export const AuthPage = () => (
-  <Suspense fallback={<div>Loading...</div>}>
+  <Suspense fallback={<FallBack />}>
     <Auth />
   </Suspense>
 );
+
+
+function FallBack (){
+  return(
+    <div className="fall-back-wrapper">
+      <div>
+        <img src={spinner} />
+      </div>
+    </div>
+  )
+}
